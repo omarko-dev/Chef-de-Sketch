764672b0ae76aa22739863a621187799c48cb81c
Omar <omar@Omars-MacBook-Air.local>
2025-05-19 01:17:09 +0300

    Fix ReactDOM.render issue for React 19

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { SavedDishesProvider } from './components/SavedDishesContext';
 import './styles/index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <SavedDishesProvider>
       <App />
     </SavedDishesProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
